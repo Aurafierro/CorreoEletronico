13f1806437661a051e3e785518b2a84de08f6d72
Aurafierro <auramariafierrofierro@gmail.com>
2024-07-15 16:10:50 -0500

    correo

---
+++ Correos/src/main/java/com/proyectofinalCorreo/Correos/CorreosApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinalCorreo.Correos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreosApplication.class, args);
+	}
+
+}
---
+++ Correos/src/main/java/com/proyectofinalCorreo/Correos/configuration/emailConfiguration.java
@@ -0,0 +1,26 @@
+package com.proyectofinalCorreo.Correos.configuration;
+
+import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class emailConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("asignaweb@gmail.com");
+        mailSender.setPassword("tlgk zzly xloq hluw");
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        return mailSender;
+    }
+}
+
